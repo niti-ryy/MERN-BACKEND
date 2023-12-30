f76d6a81a248b567cbb82ceb6d7d36cf2b857004
Nitish <nitishoffica3@gmail.com>
2023-12-30 18:01:11 +0530

    second commit to proj-branch

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port=3099
 app.get("/api/projects",(req,res)=>{
     res.json("projects")
 })
+
+app.get("/api/projects/total",(req,res)=>{
+    res.json({total:5})
+})
 app.listen(port,()=>{
     console.log("port running successfully on port",port)
 })
