247ae0aff913c40be1f61328a7ed74fae480334f
Nitish <nitishoffica3@gmail.com>
2023-12-30 17:53:21 +0530

    commit on project branch

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require("express")
 const app=express()
 const port=3099
 
+
+app.get("/api/projects",(req,res)=>{
+    res.json("projects")
+})
 app.listen(port,()=>{
     console.log("port running successfully on port",port)
 })
