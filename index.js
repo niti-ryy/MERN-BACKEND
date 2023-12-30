25ada4867e7684b0fcda59bc1247e721539ea88a
Nitish <nitishoffica3@gmail.com>
2023-12-30 19:30:30 +0530

    auth

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get("/api/users",(req,res)=>{  //create by gravit
     res.send("creating users branchh")
 })
 
+app.post("/auth",(req,res)=>{
+    res.send("auth")
+})
+
 app.listen(port,()=>{
     console.log("port running successfully on port",port)
 })
