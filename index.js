b351c3b4a170fcf45c65d8ebc4a5cbd94e05ddfb
Nitish <nitishoffica3@gmail.com>
2023-12-30 19:27:15 +0530

    second msg

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.get("/api/projects/total",(req,res)=>{
     res.json({total:5})
 })
 
-app.get("/api/users",(req,res)=>{  //create by gravita
-    res.send("creating users branch")
+app.get("/api/users",(req,res)=>{  //create by gravit
+    res.send("creating users branchh")
 })
 
 app.listen(port,()=>{
